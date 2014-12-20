76fc9ed2b0d01a4921fc1c41362ed91c7e09815a
Rube <353371737@qq.com>
2014-12-20 20:36:33 +0800

    try to translate zh to en

--- 百度翻译/fanyi.sh
+++ 百度翻译/fanyi.sh
@@ -1,12 +1,11 @@
 #/bin/bash
-src=$1
-echo 英文:$1
+echo 中译英:zh content  ---  英译中按en content
+src=$2
+echo 文本:$2
 src=${src// /+}
-
+if [ "$1" == "en" ]; then
 s=$(curl -o temp.json -d "from=en&to=zh&query=$src" "http://fanyi.baidu.com/v2transapi" 2>>/dev/null)
-
+else
+s=$(curl -o temp.json -d "from=zh&to=en&query=$src" "http://fanyi.baidu.com/v2transapi" 2>>/dev/null)
+fi
 python ./fanyi.py
- 
-
-
-
