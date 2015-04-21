6daf63725ce14a64c575e1aa8d64a8d65e7a9041
Rube <353371737@qq.com>
2015-04-21 13:44:21 +0800

    add 文言文

--- 百度翻译/fanyi.sh
+++ 百度翻译/fanyi.sh
@@ -1,9 +1,12 @@
 #/bin/bash
 echo 中译英:zh content  ---  英译中按en content
+echo 翻译成文言文 towyw content
 src=$2
 echo 文本:$2
 src=${src// /+}
-if [ "$1" == "en" ]; then
+if [ "$1" == "towyw" ]; then
+s=$(curl -o temp.json -d "from=zh&to=wyw&query=$src" "http://fanyi.baidu.com/v2transapi" 2>>/dev/null)
+elif [ "$1" == "en" ]; then
 s=$(curl -o temp.json -d "from=en&to=zh&query=$src" "http://fanyi.baidu.com/v2transapi" 2>>/dev/null)
 else
 s=$(curl -o temp.json -d "from=zh&to=en&query=$src" "http://fanyi.baidu.com/v2transapi" 2>>/dev/null)
